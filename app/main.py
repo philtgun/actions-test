7b18e09ce279de98477c86db6acc74a7976f1b6b
Philip Tovstogan <phil.tgun@gmail.com>
2020-12-28 18:10:14 +0100

    Add pre-commit and newer python features

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
 import numpy
+from dataclasses import dataclass
 
 
-def add(x, y):
+def add(x: int, y: int) -> int:
     return x + y
+
+
+def add_arrays(x: numpy.ndarray, y: numpy.ndarray) -> numpy.ndarray:
+    return x + y
+
+
+def add_lists(x: list[int], y: list[int]) -> list[int]:
+    return x + y
+
+
+@dataclass
+class Location:
+    x: float
+    y: float
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,24 @@
-from app.main import add
+from app.main import add, add_arrays, Location, add_lists
+import numpy as np
 
 
 class TestMain:
     def test_add(self):
         assert add(2, 2) == 4
+
+    def test_add_np(self):
+        x = np.array([1, 2])
+        y = np.array([3, 4])
+        result = add_arrays(x, y)
+        assert result[0] == 4 and result[1] == 6
+
+    def test_location(self):
+        location1 = Location(0.1, 100)
+        location2 = Location(0.1, 100)
+        assert location1 == location2
+        assert location1 is not location2
+
+    def test_add_lists(self):
+        x = [1, 2]
+        y = [3, 4]
+        assert add_lists(x, y) == [1, 2, 3, 4]
